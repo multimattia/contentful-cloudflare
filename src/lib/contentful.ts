0c852d4c076afe276c241ed5ab8f9b6031d39975
maat <matt@mattchan.xyz>
2024-08-27 13:16:40 -0400

    initial commit

---
+++ src/lib/contentful.ts
@@ -0,0 +1,24 @@
+import contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
+
+export interface BlogPost {
+  contentTypeId: "blogPost";
+  fields: {
+    title: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    body: EntryFieldTypes.Text;
+    richbody: EntryFieldTypes.RichText;
+    publishDate: EntryFieldTypes.Date;
+    content: EntryFieldTypes.Text;
+    author: EntryFieldTypes.Text[];
+    tags: EntryFieldTypes.Text[];
+  };
+}
